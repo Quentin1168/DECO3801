b142d35e804b1febe37bf77ed68ea07a4a159b96
J Liu <s4717707@student.uq.edu.au>
2022-08-23 00:42:07 +0800

    Modified MainActivity and layout

--- app/src/main/java/com/example/deco3801project/MainActivity.java
+++ app/src/main/java/com/example/deco3801project/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.deco3801project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void handleText(View v) {
+        TextView ageInput = findViewById(R.id.ageText);
+        String age = ageInput.getText().toString(); // Get age
+        Log.d("info", age);
     }
 }
