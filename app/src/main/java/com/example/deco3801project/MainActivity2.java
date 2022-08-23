6b323582274b415bb206b3d301654e57aff8a163
LeSebring <jasonloe02@yahoo.com>
2022-08-24 00:40:34 +0700

    Reads and displays the user's input age and gender
    - Reads the user's age and gender and stores them into corresponding variables on the second screen
    - Displays the user's input age and gender on the second screen as TextViews for debugging purposes

--- app/src/main/java/com/example/deco3801project/MainActivity2.java
+++ app/src/main/java/com/example/deco3801project/MainActivity2.java
@@ -2,13 +2,29 @@ package com.example.deco3801project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        pref = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
         setContentView(R.layout.activity_main2);
+
+        // Retrieves the user's input age and gender
+        String age = pref.getString("age", null);
+        String gender = pref.getString("gender", null);
+
+        // Displays the user's age and gender for debugging purposes
+        TextView ageInput = findViewById(R.id.ageText);
+        ageInput.setText(age);
+        TextView genderInput = findViewById(R.id.genderText);
+        genderInput.setText(gender);
     }
 }
