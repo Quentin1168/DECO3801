0ec209936838a40aace1d592315a06c74579914d
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-08-24 22:33:09 +0100

    Allow only integer input of age
    set in activity_main.xml, no code added

--- app/src/main/java/com/example/deco3801project/MainActivity.java
+++ app/src/main/java/com/example/deco3801project/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.deco3801project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
+import java.lang.String;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,8 +35,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+    public void handleAge(View v) {
+        TextView input = findViewById(R.id.ageText);
+        String ageFormat = input.getText().toString();
+        if (!ageFormat.matches("[0-9]+")) {
+            System.out.println("Invalid number");
+        }
+    }
+     */
+
     public void handleText(View v) { // This method run when button is clicked
         TextView ageInput = findViewById(R.id.ageText);
+        //ageInput.setInputType(InputType.TYPE_CLASS_NUMBER);
         String age = ageInput.getText().toString(); // Get age
         Log.d("info", age);
 
